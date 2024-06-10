534a71f9080ebcac9989dbd68486ffd4ec80bf1b
Ramon Lorenzo <ramoonlorenzo@gmail.com>
2024-06-10 17:08:11 -0300

    Update folders structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,31 @@
 const button = document.querySelector(".btn-mostrar-projetos");
-const inactiveProject = document.querySelectorAll(".projeto:not(.ativo)");
 
-button.addEventListener("click", () => {
-  mostrarMaisProjetos();
+let quantidadeDeProjetos = 4;
 
-  esconderBotao();
+document.addEventListener("DOMContentLoaded", function () {
+  mostrarProjetos(quantidadeDeProjetos);
 });
 
-function esconderBotao() {
-  button.classList.add("remover");
+button.addEventListener("click", mostrarMaisProjetos);
+
+function mostrarProjetos(quantidadeDeProjetos) {
+  const projetos = document.querySelectorAll(".projeto");
+
+  for (let i = 0; i < quantidadeDeProjetos; i++) {
+    if (projetos[i]) {
+      projetos[i].classList.add("ativo");
+    }
+  }
 }
 
 function mostrarMaisProjetos() {
-  inactiveProject.forEach((projetoInativo) => {
-    projetoInativo.classList.toggle("ativo");
-  });
+  const quantidadeTotalProjetos = document.querySelectorAll(".projeto").length;
+
+  quantidadeDeProjetos += 2;
+
+  mostrarProjetos(quantidadeDeProjetos);
+
+  if (quantidadeDeProjetos >= quantidadeTotalProjetos) {
+    button.classList.add("remover");
+  }
 }
